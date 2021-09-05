a73b0ae7ba511a9568f5269dc2d6efb0ec923819
Balasubramanian Palaniappan <b.palaniappan@nationwide.com>
2021-09-05 09:09:50 -0500

    added new components

---
+++ src/components/TweetItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div @click="selectedTweet(tweet.id)">
+    @{{ username }} - {{ tweet.message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TweetItem",
+  props: {
+    username: {
+      type: String,
+      required: true
+    },
+    tweet: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    selectedTweet(id) {
+      console.log(`selected tweet is ${id}`)
+      this.$emit('selectedTweet', id)
+    }
+  }
+}
+</script>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="profile">
+    @{{ user.username }} -- {{ fullName }}
+    <br>
+    <strong>Followers: {{ followers }}</strong>
+    <br>
+    <button @click="followUser">Follow</button>
+    <br>
+    <!-- Enable only if isAdmin is true -->
+    <div v-if="user.isAdmin">
+      <strong>Admin</strong>
+    </div>
+    <div v-else>
+      <strong>User</strong>
+    </div>
+    <hr>
+    <div><strong>Tweets :</strong></div>
+
+    <!-- @selectedTweet have to match the event name from the $emit -->
+    <tweet-item v-for="tweet in user.tweets" :key="tweet.id" :username="user.username" :tweet="tweet" @selectedTweet="toggleSelected" />
+  </div>
+</template>
+
+<script>
+import TweetItem from "./TweetItem.vue"
+
+export default {
+  name: 'App',
+  components: { TweetItem },
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_balap',
+        firstName: 'Balasubramanian',
+        lastName: 'Palaniappan',
+        email: 'bala@c12.io',
+        isAdmin: true,
+        tweets: [
+          {id: 1, message: "Hello world our first message"},
+          {id: 2, message: "How is the second message going on"}
+        ]
+      }
+    }
+  },
+  watch: {
+    // watch datachange on the data and react based on it.
+    followers(newValue, oldValue) {
+      if (newValue > oldValue) {
+        console.log(`${this.user.username} gained a follower. Enjoy !!!`)
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    // general methods
+    followUser() {
+      this.followers++
+    },
+    toggleSelected(id) {
+      console.log(`selected Tweet is #${id}`)
+    }
+  },
+  mounted() {
+    // when a component is loaded.
+    // Its a life cycle method
+    console.log('component is loaded . . .');
+    this.followUser();
+  }
+}
+</script>
