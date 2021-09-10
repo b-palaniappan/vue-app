df3eee9a2b6c735bd2af9f73a5e5e5f6ed642154
Balasubramanian Palaniappan <b.palaniappan@nationwide.com>
2021-09-09 21:23:57 -0500

    updated comments in vuex

--- src/store/User.js
+++ src/store/User.js
@@ -6,6 +6,7 @@ export const UserModule = {
   },
 
   // are functions that effect the state. Cant update the state directly, only mutations can do it
+  // Mutations are only for sync operations.
   mutations: {
     SET_USER(state, user) {
       state.user = user
@@ -13,6 +14,7 @@ export const UserModule = {
   },
 
   // Action are the functions that we call through our applicaion that call mutations.
+  // For any async operations like calling api to get data have to be done using actions only. Action can call mutations to update the state
   actions: {
     setUser({ commit }, user) {
       commit('SET_USER', user)
